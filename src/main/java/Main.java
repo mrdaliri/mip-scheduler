20f288e5e80356a479eca3881efc8297f5d86200
Mohammad-Reza <mr.mohrd@gmail.com>
2019-06-19 20:05:00 +0430

    Fix Main.read function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,19 +9,18 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         try {
-            DirectedGraph<Node, String> instance = read("input.json");
+            Input instance = read("input.json");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
 
-    public static DirectedGraph<Node, String> read(String path) throws FileNotFoundException {
+    public static Input read(String path) throws FileNotFoundException {
         BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
         Gson gson = new GsonBuilder()
                 .registerTypeAdapter(new TypeToken<Input>(){}.getType(), new InputDeserializer())
                 .create();
-        Input c = gson.fromJson(bufferedReader, Input.class);
-        return null;
+        return gson.fromJson(bufferedReader, Input.class);
     }
 }
