3cb919725c78b1122d2763ac48b006f72183e1c1
Mohammad-Reza <mr.mohrd@gmail.com>
2019-07-08 17:48:29 +0430

    Use DeferredResult to run MIP computation in another thread than request one

--- src/main/java/api/ProblemController.java
+++ src/main/java/api/ProblemController.java
@@ -2,15 +2,31 @@ package api;
 
 import ilog.concert.IloException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.async.DeferredResult;
 import solver.Model;
+import solver.Node;
 import solver.Problem;
+import solver.Resource;
+
+import java.util.Map;
+import java.util.concurrent.ForkJoinPool;
 
 @RestController
 public class ProblemController {
-    @RequestMapping(value = "/solve", method = RequestMethod.POST)
-    public String Solve(@RequestBody Problem input) throws IloException {
-        Model model = new Model(input);
-        model.solve();
-        return model.getSolution().toString();
+    @PostMapping(value = "/solve")
+    public DeferredResult<Map<Node, Resource>> Solve(@RequestBody Problem input) throws IloException {
+        DeferredResult<Map<Node, Resource>> output = new DeferredResult<>();
+
+        ForkJoinPool.commonPool().submit(() -> {
+            try {
+                Model model = new Model(input);
+                model.solve();
+                output.setResult(model.getSolution());
+            } catch (IloException e) {
+                e.printStackTrace();
+            }
+        });
+
+        return output;
     }
 }
