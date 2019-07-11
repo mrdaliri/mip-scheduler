95f06473fe124c5c82d24ecf209ae6bc79acdfe1
Mohammad-Reza <mr.mohrd@gmail.com>
2019-07-11 10:18:53 +0430

    Add CORS headers

--- src/main/java/api/ProblemController.java
+++ src/main/java/api/ProblemController.java
@@ -14,6 +14,7 @@ import java.util.Map;
 import java.util.concurrent.ForkJoinPool;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ProblemController {
     @PostMapping(value = "/solve")
     public DeferredResult<Map<Node, Resource>> Solve(@RequestBody Problem input) throws IloException {
